2254fb3d119cdcfdd754d1384a9d884314a6dbd2
rkubik-navex <rkubik@navexglobal.com>
2017-08-19 13:26:39 -0700

    Create Sprite object

--- src/game.js
+++ src/game.js
@@ -4,12 +4,18 @@ window.onload = function() {
 
 var canvas;
 var context;
+var testImage;
+var testSprite;
 
 var init = function() {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
     canvas.height = 500;
     canvas.width = 500;
+
+    testImage = new Image();
+    testImage.src = "testasset.png";
+    testSprite = new Sprite(testImage);
 }
 
 var update = function(delta) {
@@ -19,6 +25,7 @@ var update = function(delta) {
 var render = function () {
     context.fillStyle = 'green';
     context.fillRect(10, 10, 100, 500);
+    testSprite.render(context);
 }
 
 var now = Date.now();
@@ -36,3 +43,15 @@ var tick = function() {
 var interval = 1000 / 60;
 
 setInterval(tick, interval);
+
+var Sprite = function(image) {
+    this.image = image;
+    this.height = 16;
+    this.width = 16;
+    this.x = 0;
+    this.y = 0;
+
+    this.render = function(context) {
+        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+}
